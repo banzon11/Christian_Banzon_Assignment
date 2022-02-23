1412772c51ede8336669f3689c3ee11f1b2de7e5
Christian <cdbanzon@up.edu.ph>
2022-02-23 20:55:22 +0800

    feat: fix error in heroku requirements

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '0.0.0.0'
+    'christianassignment.herokuapp.com',
+    '127.0.0.1']
 
 
 # Application definition
